03647623a938398912b354760e58eb5c3ea2db19
Atharva Deosthale <atharva.deosthale17@gmail.com>
2022-07-21 20:14:37 +0530

    Initial commit:

---
+++ components/PaymentForm.js
@@ -0,0 +1,67 @@
+import {
+  CardElement,
+  PaymentElement,
+  PaymentRequestButtonElement,
+  useElements,
+  useStripe,
+} from "@stripe/react-stripe-js";
+import React, { useState } from "react";
+
+function PaymentForm() {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const stripe = useStripe();
+  const elements = useElements();
+
+  const createSubscription = async () => {
+    try {
+      const paymentMethod = await stripe.createPaymentMethod({
+        card: elements.getElement("card"),
+        type: "card",
+      });
+      const response = await fetch("/api/subscribe", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          name,
+          email,
+          paymentMethod: paymentMethod.paymentMethod.id,
+        }),
+      });
+      if (!response.ok) return alert("Payment unsuccessful!");
+      const data = await response.json();
+      const confirm = await stripe.confirmCardPayment(data.clientSecret);
+      if (confirm.error) return alert("Payment unsuccessful!");
+      alert("Payment Successful! Subscription active.");
+    } catch (err) {
+      console.error(err);
+      alert("Payment failed! " + err.message);
+    }
+  };
+
+  return (
+    <div style={{ width: "40%" }}>
+      Name:{" "}
+      <input
+        type="text"
+        value={name}
+        onChange={(e) => setName(e.target.value)}
+      />
+      <br />
+      Email:{" "}
+      <input
+        type="email"
+        value={email}
+        onChange={(e) => setEmail(e.target.value)}
+      />
+      <br />
+      <CardElement />
+      <br />
+      <button onClick={createSubscription}>Subscribe - 5 INR</button>
+    </div>
+  );
+}
+
+export default PaymentForm;
---
+++ pages/api/subscribe.js
@@ -0,0 +1,52 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import Stripe from "stripe";
+
+const stripe = new Stripe(process.env.STRIPE_SECRET_KEY);
+
+export default async function handler(req, res) {
+  try {
+    if (req.method != "POST") return res.status(400);
+    const { name, email, paymentMethod } = req.body;
+    // Create a customer
+    const customer = await stripe.customers.create({
+      email,
+      name,
+      payment_method: paymentMethod,
+      invoice_settings: { default_payment_method: paymentMethod },
+    });
+    // Create a product
+    const product = await stripe.products.create({
+      name: "Monthly subscription",
+    });
+    // Create a subscription
+    const subscription = await stripe.subscriptions.create({
+      customer: customer.id,
+      items: [
+        {
+          price_data: {
+            currency: "INR",
+            product: product.id,
+            unit_amount: "500",
+            recurring: {
+              interval: "month",
+            },
+          },
+        },
+      ],
+
+      payment_settings: {
+        payment_method_types: ["card"],
+        save_default_payment_method: "on_subscription",
+      },
+      expand: ["latest_invoice.payment_intent"],
+    });
+    // Send back the client secret for payment
+    res.json({
+      message: "Subscription successfully initiated",
+      clientSecret: subscription.latest_invoice.payment_intent.client_secret,
+    });
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ message: "Internal server error" });
+  }
+}
